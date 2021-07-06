1f9586dcedd928033bd2500228ac5e9f90eb6a34
Guilherme Sales <guilherme.sales@contabilizei.com.br>
2021-07-06 18:10:09 -0300

    refactor(Account): Add service layer in order to uncouple controller

--- src/main/kotlin/com/br/bank/model/Account.kt
+++ src/main/kotlin/com/br/bank/model/Account.kt
@@ -1,4 +1,4 @@
-package com.br.bank
+package com.br.bank.model
 
 import javax.persistence.Entity
 import javax.persistence.GeneratedValue
--- src/main/kotlin/com/br/bank/repository/AccountRepository.kt
+++ src/main/kotlin/com/br/bank/repository/AccountRepository.kt
@@ -1,5 +1,6 @@
-package com.br.bank
+package com.br.bank.repository
 
+import com.br.bank.model.Account
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface AccountRepository: JpaRepository<Account, Long> {
